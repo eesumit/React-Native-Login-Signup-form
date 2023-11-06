86be3f6ff7558c6d456e3f9c4365343adfbe68de
SUMIT <112788729+eesumit@users.noreply.github.com>
2023-11-07 00:27:19 +0530

    Update App.js
    updating the path of other connected files

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';// you have to install this navigation dependency from react-native official website.
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/Home';
-import Signup from './src/Signup';
-import Login from './src/Login';
+import Home from './Home';
+import Signup from './Signup';
+import Login from './Login';
 
 const Stack = createNativeStackNavigator();
  
